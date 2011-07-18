40ba36a08f810ae8e0a0cd984e83f97d61fd41ba
jdomingo <jdomingo@green-code.com>
2011-07-18 15:14:22 +0000

    Changed version to 0.82

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("LANDIS Spatial Modeling Library")]
-[assembly: AssemblyVersion("0.81.*")]
+[assembly: AssemblyVersion("0.82.*")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
