205ad6189a4211b5c6417da6f8dc419f48fff26c
jdomingo <jdomingo@green-code.com>
2012-06-22 00:18:33 +0000

    Updated the link to the BSD license page.  According to the Wayback Machine, between May 14 and June 5, OSI replaced the single BSD page with 2 pages (one for 2-Clause, another for 3-Clause).  See these threads for details:
      http://www.mail-archive.com/license-discuss@opensource.org/msg08242.html
      http://www.mail-archive.com/license-discuss@opensource.org/msg08173.html
    
    The original link now points to the Simplified BSD 2-Clause, so had to replace it with the link to the New BSD 3-Clause.

--- src/CoreServices/Landscapes/DataIndexes/Array2D.cs
+++ src/CoreServices/Landscapes/DataIndexes/Array2D.cs
@@ -5,7 +5,7 @@
 // License (the "License").  You may not use this file except in
 // compliance with the License.  A copy of the License is available at
 //
-//   http://www.opensource.org/licenses/bsd-license.php
+//   http://www.opensource.org/licenses/BSD-3-Clause
 //
 // and is included in the NOTICE.txt file distributed with this work.
 //
