156d5f2e15afb42460016321acd05c6704bba504
jdomingo <jdomingo@green-code.com>
2012-07-05 04:32:55 +0000

    Completed the Windows command script to fetch the remaining tools in the toolkit.

--- tools/WinPkg/dist/clients/get-tools.cmd
+++ tools/WinPkg/dist/clients/get-tools.cmd
@@ -1,5 +1,36 @@
 @echo off
 
+rem  Run this script in its folder
+pushd %~dp0
+
 setlocal
 set ScriptURL=$HeadURL$
-echo This script's URL = '%ScriptURL%'
+rem  Remove leading and trailing delimiters of svn keyword from the URL
+set ScriptURL=%ScriptURL:$HeadURL: =%
+set ScriptURL=%ScriptURL: $=%
+
+set ToolkitRootURL=%ScriptURL:/clients/get-tools.cmd=%
+set DownloadURL=%ToolkitRootURL%/download
+
+call :getFile checksum.exe
+call :getFile checksum-LICENSE.txt
+call :getFile unzip.exe
+call :getFile Info-ZIP-LICENSE.txt
+
+popd
+goto :eof
+
+rem  -------------------------------------------------------------------------
+
+:getFile
+
+set FileName=%1
+if exist %FileName% (
+  echo %FileName% already downloaded
+  goto :eof
+)
+echo Downloading %FileName% ...
+.\Landis.Tools.DownloadFile.exe %DownloadURL%/%FileName% %FileName%
+goto :eof
+
+rem  -------------------------------------------------------------------------
