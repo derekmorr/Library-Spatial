3aa4dd74bac7f946176d1be6c97cf86ce390662d
jdomingo <jdomingo@green-code.com>
2012-08-22 21:13:19 +0000

    Refactored the LSML-admin.cmd script in the svn example, so it now accepts an action argument.  The "get" action performs the existing functionality (downloading and unpacking a LSML release).  Added a new "help" action, which is the default if no action is specified.

--- examples/svn/third-party/LSML/LSML-admin.cmd
+++ examples/svn/third-party/LSML/LSML-admin.cmd
@@ -4,6 +4,14 @@ rem  Run this script in the directory where it's located
 pushd %~dp0
 setlocal
 
+set Script=%~nx0
+call :processArgs %*
+if "%Action%" == "error" goto :exitScript
+if "%Action%" == "help" (
+  call :usage
+  goto :exitScript
+)
+
 rem  Read LSML version # and the SHA1 checksum for that version
 for /f "tokens=1,2" %%i in (version.txt) do (
   set LibraryVer=%%i
@@ -20,4 +28,61 @@ set FileInPkg=Landis.SpatialModeling.dll
 
 call WinPkgTools\getPackage.cmd %LibraryUrl% %LibraryPackage% %LibrarySHA1% %FileInPkg%
 
+
+:exitScript
+
 popd
+
+set ExitCode=0
+if "%Action%" == "error" (
+  set ExitCode=1
+) else (
+  set ExitCode=0
+)
+exit /b %ExitCode%
+
+rem  ------------------------------------------------------------------------
+
+:processArgs
+
+set Action=
+if "%~1" == "get"  set Action=get
+if "%~1" == "help" set Action=help
+if "%~1" == ""     set Action=help
+
+if "%Action%" == "" (
+  call :error unknown action "%~1"
+  goto :eof
+)
+if not "%~3" == "" (
+  call :error extra arguments after "%~1" action: %2 ...
+  goto :eof
+)
+if not "%~2" == "" (
+  call :error extra argument after "%~1" action: %2
+  goto :eof
+)
+
+goto :eof
+
+rem  ------------------------------------------------------------------------
+
+:error
+
+echo Error: %*
+call :usage
+set Action=error
+goto :eof
+
+rem  ------------------------------------------------------------------------
+
+:usage
+
+echo Usage: %Script% [ACTION]
+echo where ACTION is:
+echo   get  -- download and unpack LSML
+echo   help -- display this message (default)
+
+goto :eof
+
+rem  ------------------------------------------------------------------------
