81c68e77a3c3fa79caa05e693a18efa990c97c18
jdomingo <jdomingo@green-code.com>
2012-08-29 19:28:19 +0000

    Advanced the library's version to 0.85

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("LANDIS Spatial Modeling Library")]
-[assembly: AssemblyVersion("0.84.*")]
+[assembly: AssemblyVersion("0.85.*")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
