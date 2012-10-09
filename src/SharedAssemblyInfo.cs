cabbcb5bd6eb31762b0ca3f4792a0bbcd3a9d610
jdomingo <jdomingo@green-code.com>
2012-10-09 16:48:47 +0000

    Advanced the library's version to 0.91. Extensive testing with LANDIS-II has confirmed that the library is stable enough to become a release candidate.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("LANDIS Spatial Modeling Library")]
-[assembly: AssemblyVersion("0.86.*")]
+[assembly: AssemblyVersion("0.91.*")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
