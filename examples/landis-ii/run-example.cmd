6bc565802c368650cea1f3d26eb25a0e8d048fb8
jdomingo <jdomingo@green-code.com>
2012-10-10 03:04:12 +0000

    Revised the Windows script for the LANDIS-II example to handle the situation where the GDAL_BIN_DIR environment variable isn't set.

--- examples/landis-ii/run-example.cmd
+++ examples/landis-ii/run-example.cmd
@@ -1,6 +1,9 @@
 @echo off
 
 setlocal
+set SCRIPT_DIR=%~dp0
+set PROJECT_ROOT=%SCRIPT_DIR:\examples\landis-ii\=%
+
 if "%1" == "debug" goto checkEnvVar
 if "%1" == "release" goto checkEnvVar
 if "%1" == "" (
@@ -14,10 +17,12 @@ goto :eof
 rem -----------------------------------------------------------------
 
 :checkEnvVar
-if "%GDAL_BIN_DIR%" == "" (
-  echo Error: GDAL_BIN_DIR environment variable is not set
-  goto :eof
-)
+if not "%GDAL_BIN_DIR%" == "" goto envVarSet
+set GDAL_BIN_DIR=%PROJECT_ROOT%\external\gdal\libs\native
+echo GDAL_BIN_DIR environment variable is not set; using default:
+echo    %GDAL_BIN_DIR%
+
+:envVarSet
 if not exist "%GDAL_BIN_DIR%" (
   echo Error: GDAL_BIN_DIR environment variable refers to non-existant directory
   goto :eof
@@ -32,8 +37,7 @@ echo.
 set PATH_SAVE=%PATH%
 PATH %GDAL_BIN_DIR%;%PATH%
 
-set SCRIPT_DIR=%~dp0
-set CONFIG_DIR=%SCRIPT_DIR%..\..\build\%~1
+set CONFIG_DIR=%PROJECT_ROOT%\build\%~1
 set EXE_PATH=%CONFIG_DIR%\LandisII.Examples.Console.exe
 "%EXE_PATH%"
 
