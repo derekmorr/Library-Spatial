f5854ec136cab8665635de399404c26db6e5821b
jdomingo <jdomingo@green-code.com>
2013-03-08 04:05:13 +0000

    Advanced version from 0.91 to 1.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("LANDIS Spatial Modeling Library")]
-[assembly: AssemblyVersion("0.91.*")]
+[assembly: AssemblyVersion("1.0.*")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
