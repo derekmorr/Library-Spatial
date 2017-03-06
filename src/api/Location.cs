1cc152dd59e07e380a6ff0407c99070ed54f94a7
Robert Scheller <rmscheller@gmail.com>
2017-03-06 13:05:06 -0800

    Update Location.cs

--- src/api/Location.cs
+++ src/api/Location.cs
@@ -1,14 +1,6 @@
 // Copyright 2004-2006,2010 University of Wisconsin
 // All rights reserved. 
 //
-// The copyright holders license this file under the New (3-clause) BSD
-// License (the "License").  You may not use this file except in
-// compliance with the License.  A copy of the License is available at
-//
-//   http://www.opensource.org/licenses/BSD-3-Clause
-//
-// and is included in the NOTICE.txt file distributed with this work.
-//
 // Contributors:
 //   James Domingo, UW-Madison, Forest Landscape Ecology Lab
 //   James Domingo, Green Code LLC
