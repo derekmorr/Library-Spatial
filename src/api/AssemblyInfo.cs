1b5a32f2146848b52a7473578e816ceed8346b2c
Derek Morr <derekmorr@psu.edu>
2017-11-27 15:05:31 -0500

    update to .NET Core 2.0 / VS2017 style csproj

--- src/api/AssemblyInfo.cs
+++ src/api/AssemblyInfo.cs
@@ -1,7 +1,4 @@
-using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyDescription("Library API")]
 
 [assembly: InternalsVisibleTo("Landis.Landscapes")]
 // So private methods of site-related structs can be utilized
